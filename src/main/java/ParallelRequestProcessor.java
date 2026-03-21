0d171ab05d137c4e73a5dc5a294992cd38c801ba
Pranjal Sharma <pranjalsharma@Pranjals-MacBook-Air.local>
2026-03-21 22:11:13 +0530

    codecrafters submit [skip ci]

--- src/main/java/ParallelRequestProcessor.java
+++ src/main/java/ParallelRequestProcessor.java
@@ -2,16 +2,20 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+
 
 @Setter
 @Getter
 public class ParallelRequestProcessor implements Runnable {
     private InputStream in;
     private OutputStream out;
+    private final Logger LOG = LogManager.getLogger(ParallelRequestProcessor.class);
     @Override
     public void run() {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(in));
@@ -22,6 +26,13 @@ public class ParallelRequestProcessor implements Runnable {
                 if("PING".equals(line)) {
                     out.write("+PONG\r\n".getBytes());
                     out.flush();
+                }
+                else if("ECHO".equals(line)) {
+                    while(line!=null) {
+                        line = bufferedReader.readLine();
+                        out.write((line+"\r\n").getBytes());
+                        LOG.info(line);
+                    }
                 }
             } catch (IOException e) {
                 throw new RuntimeException(e);
