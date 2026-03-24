bbde9c69c096c2c13aa0dc14abdccc48465761c2
Pranjal Sharma <pranjalsharma@Pranjals-MacBook-Air.local>
2026-03-24 22:40:15 +0530

    codecrafters submit [skip ci]

--- src/main/java/commands/LRangeCommand.java
+++ src/main/java/commands/LRangeCommand.java
@@ -21,19 +21,24 @@ public class LRangeCommand implements IListCommand{
         List<String> s = listMap.getOrDefault(command, new ArrayList<>());
         int start = Integer.parseInt(reader.read());
         int stop = Math.min(Integer.parseInt(reader.read()), s.size()-1);
+        if(start<-s.size()) start=0;
+        if(stop<-s.size()) stop=0;
+        if(start<0) start=s.size()+start;
+        if(stop<0) stop = s.size()+stop;
         log.info("Start: "+start+"Stop: "+stop);
         log.info("ListSize: "+s.size());
         if(!listMap.containsKey(command) || start>stop || start>s.size()-1) {
             out.write("*0\r\n".getBytes());
             return;
         }
+        List<String> queryList = s.subList(start, stop+1);
         int numElements = stop-start+1;
         log.info("numele: "+numElements);
         StringBuilder sb = new StringBuilder();
         sb.append("*").append(numElements).append("\r\n");
-        for(;start<=stop;start++) {
-            sb.append("$").append(s.get(start).length()).append("\r\n").append(s.get(start)).append("\r\n");
-        }
+        queryList.forEach((element)->{
+            sb.append("$").append(element.length()).append("\r\n").append(element).append("\r\n");
+        });
 //        sb.append("\r\n");
         out.write(sb.toString().getBytes());
     }
