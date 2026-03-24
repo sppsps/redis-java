5d11dddec6aa4929cfb1dfe9798a5f191757e2c3
Pranjal Sharma <pranjalsharma@Pranjals-MacBook-Air.local>
2026-03-24 19:26:47 +0530

    codecrafters submit [skip ci]

--- src/main/java/dto/StringReader.java
+++ src/main/java/dto/StringReader.java
@@ -1,10 +1,14 @@
 package dto;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 
 public class StringReader {
     public BufferedReader reader;
+    private Logger log = LogManager.getLogger(StringReader.class);
 
     public StringReader(BufferedReader bufferedReader) {
         this.reader = bufferedReader;
@@ -12,8 +16,11 @@ public class StringReader {
 
     public String read() throws IOException {
         String line = reader.readLine();
+        log.info("Line 1: "+line);
         if(line==null) return null;
+
         line = reader.readLine();
+        log.info("line2: "+line);
         return line;
     }
 }
