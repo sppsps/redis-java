41d9effa88f360ef3b46ecb5cd6b90abe87c9837
Pranjal Sharma <pranjalsharma@Pranjals-MacBook-Air.local>
2026-05-24 02:47:56 +0530

    codecrafters submit [skip ci]

--- src/main/java/commands/PsyncCommand.java
+++ src/main/java/commands/PsyncCommand.java
@@ -6,6 +6,8 @@ import dto.StringReader;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.Arrays;
+import java.util.Base64;
 
 public class PsyncCommand implements ReplicationCommand{
     @Override
@@ -14,5 +16,9 @@ public class PsyncCommand implements ReplicationCommand{
         String replicationId = stringReader.read();
         String replicaOffset = stringReader.read();
         out.write("+FULLRESYNC 8371b4fb1155b71f4a04d3e1bc3e18c4a990aeeb 0\r\n".getBytes());
+        String emptyRDBFile = new String("UkVESVMwMDEx+glyZWRpcy12ZXIFNy4yLjD6CnJlZGlzLWJpdHPAQPoFY3RpbWXCbQi8ZfoIdXNlZC1tZW3CsMQQAPoIYW9mLWJhc2XAAP/wbjv+wP9aog==");
+        byte[] rdbFileBytes = Base64.getDecoder().decode(emptyRDBFile);
+        out.write(("$"+rdbFileBytes.length+"\r\n").getBytes());
+        out.write(rdbFileBytes);
     }
 }
